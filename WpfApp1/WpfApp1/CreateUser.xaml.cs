d9285b5b1f1c1845810d2d2b42d514018864bbb6
Klausiboy <Klaus@DESKTOP-CEDJEJ0>
2018-01-11 12:04:40 +0100

    Made Window design, started code behind

---
+++ WpfApp1/WpfApp1/CreateUser.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for CreateUser.xaml
+    /// </summary>
+    public partial class CreateUser : Window
+    {
+        public CreateUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            bool status;
+            //Call DBMethod
+            status = SaveUser();
+            if (true)
+            {
+
+            }
+        }
+
+        private bool SaveUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
